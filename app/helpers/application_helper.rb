6fe18a0ac9193f7992ce71cf487c823d76fd8c1a
mdobrota <mdobrota@tribune.com>
2012-02-04 15:08:30 -0600

    user login header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def user_header
+    if current_user
+      output = "Welcome #{current_user.name_or_email} | "
+      output << link_to('Sign Out', destroy_user_session_path)
+      output.html_safe
+    else
+      output = link_to('Sign in', new_user_session_path)
+      output.html_safe
+    end
+  end
 end
