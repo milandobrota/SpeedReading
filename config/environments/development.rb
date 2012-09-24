3ad700e01cb3c327f17eec305ef75e0ad44fdab4
mdobrota <mdobrota@tribune.com>
2012-09-23 22:30:58 -0500

    prevent compiled assets from being served in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,6 @@ SpeedReading::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.assets.prefix = '/assets_dev'
 end
